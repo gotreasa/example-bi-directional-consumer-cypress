f4814a345906581ab1561ffe3bd25db958bc07e4
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-02-24 02:15:01 +0000

    feat: upgrade cypress to latest version

---
+++ cypress.config.cjs
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    headersBlocklist: ["ignore-me-globally"],
+    ignoreDefaultBlocklist: false,
+  },
+
+  retries: 3,
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
